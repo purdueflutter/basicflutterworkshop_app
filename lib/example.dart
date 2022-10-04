197d8d253322246f2a4e6729427b4516d1116275
Geoffrey Cramer <gcwill70@gmail.com>
2022-10-04 11:12:24 -0400

    classcontainer

--- lib/example.dart
+++ lib/example.dart
@@ -93,7 +93,110 @@ class _ExamplePageState extends State<ExamplePage> {
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "Classes Today",
+              style: TextStyle(
+                fontSize: 28,
+                color: darkBlueColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          ClassContainer(
+            date: "10:30 AM",
+            subject: "CS180 - Obj Oriented Programming",
+          ),
+          SizedBox(height: 5.0),
+          ClassContainer(
+            date: "1:30 PM",
+            subject: "CS250 - Microeconomics",
+          ),
+          SizedBox(height: 5.0),
+          ClassContainer(
+            date: "3:30 PM",
+            subject: "MA251 - Multivariate Calculus",
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class ClassContainer extends StatelessWidget {
+  final String subject;
+  final String date;
+
+  const ClassContainer({
+    Key? key,
+    required this.subject,
+    required this.date,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: SizedBox(
+        height: 100,
+        width: MediaQuery.of(context).size.width * 8,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: pinkColor.withOpacity(0.4),
+                offset: Offset(10, 10),
+              )
+            ],
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: 40,
+                  width: 120,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: pinkColor,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    date,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    alignment: Alignment.center,
+                    child: Text(
+                      subject,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: darkBlueColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
